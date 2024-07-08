d340d406ad363016d22863392d1b7c3918035310
Divya Sharma <122516735+divyasharma82@users.noreply.github.com>
2024-07-08 13:19:23 +0530

    implement initial layout and folder  structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@mui/material';
+import Header from './components/Header/Header.jsx';
+import List from './components/List/List.jsx';
+import Map from './components/Map/Map.jsx';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
